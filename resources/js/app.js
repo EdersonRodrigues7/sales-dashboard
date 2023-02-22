bdd4d11dc5731551c9c14016a03a3a2bc7cf4e7e
Ederson Rodrigues <ederson@trilhante.com.br>
2023-02-22 12:57:17 -0300

    :sparkles: feat: add dashboard scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import dashboard from './components/dashboard';
 
 window.Alpine = Alpine;
 
+Alpine.data('dashboard', dashboard);
 Alpine.start();
---
+++ resources/js/components/dashboard.js
@@ -0,0 +1,24 @@
+export default () => ({
+    loading: false,
+    generateReport() {
+        this.loading = true;
+        const sizes = this.$refs.vegalitecontainer.getBoundingClientRect();
+
+        this.$wire.generateReport()
+            .then((result) => {
+                let dataset = this.$wire.get('dataset');
+
+                result.data = dataset;
+                result.height = sizes.height;
+                result.width = sizes.width;
+
+                window.vegaEmbed("#vis", result);
+
+                this.loading = false;
+            })
+            .catch((err) => {
+                this.loading = false;
+                console.log(err);
+            });
+    }
+});
